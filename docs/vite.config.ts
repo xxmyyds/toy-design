1e653e7e730a07380925bd73ded1201fefcefe75
Code_xxmyyds <xxmyyds@163.com>
2022-10-30 22:56:59 +0800

    docs: add logo

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@dist': new URL('../dist', import.meta.url).pathname,
-    },
-  },
   plugins: [vue()],
 })
