4a81e1996148cb3f6a1e1532c2533cff8d19f5d4
Code_xxmyyds <xxmyyds@163.com>
2022-11-08 23:01:51 +0800

    style: init button scss

--- play/src/App.vue
+++ play/src/App.vue
@@ -2,7 +2,11 @@
   <div class="">
     123
     <t-button type="default"></t-button>
+    <t-button type="primary"></t-button>
     <t-button type="success"></t-button>
+    <t-button type="danger"></t-button>
+    <t-button type="info"></t-button>
+    <t-button type="warning"></t-button>
     <t-input></t-input>
   </div>
 </template>
--- play/src/main.ts
+++ play/src/main.ts
@@ -3,11 +3,11 @@ import App from './App.vue'
 // import { TButton } from 'toy-design'
 
 // 开发阶段引入
-// import ToyDesign from '../../packages/toy-design/index'
-// import '../../packages/toy-theme/src/index.scss'
+import ToyDesign from '../../packages/toy-design/index'
+import '../../packages/toy-theme/src/index.scss'
 
 // 打包阶段引入
-import ToyDesign from '@dist/es'
-import '@dist/dist/index.css'
+// import ToyDesign from '@dist/es'
+// import '@dist/dist/index.css'
 
 createApp(App).use(ToyDesign).mount('#play')
