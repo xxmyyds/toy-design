19c1e3122a43c4b34bd65676fe59933d88ac3386
Code_xxmyyds <xxmyyds@163.com>
2023-01-19 13:36:22 +0800

    test(unit test): 补充组件单元测试

--- packages/toy-components/button/__tests__/button.spec.tsx
+++ packages/toy-components/button/__tests__/button.spec.tsx
@@ -1,9 +1,34 @@
 import { mount } from '@vue/test-utils'
+import { ToyIconLoading } from '@xxm7/toy-icon'
 import Button from '../src/button.vue'
 
 describe('Button', () => {
   it('create', () => {
     const wrapper = mount(() => <Button type="primary" />)
     expect(wrapper.classes()).toContain('toy-button--primary')
+  })
+
+  it('loading', () => {
+    const wrapper = mount(() => <Button loading />)
+
+    expect(wrapper.classes()).toContain('is-loading')
+    expect(wrapper.findComponent(ToyIconLoading).exists()).toBeTruthy()
+  })
+
+  it('plain', () => {
+    const wrapper = mount(() => <Button plain />)
+
+    expect(wrapper.classes()).toContain('is-plain')
+  })
+
+  it('round', () => {
+    const wrapper = mount(() => <Button round />)
+    expect(wrapper.classes()).toContain('is-round')
+  })
+
+  it('circle', () => {
+    const wrapper = mount(() => <Button circle />)
+
+    expect(wrapper.classes()).toContain('is-circle')
   })
 })
